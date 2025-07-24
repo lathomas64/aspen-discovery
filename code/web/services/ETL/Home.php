4f013542d4d8295f01bd1604035403027898825e
Imani <imani.thomas@bywatersolutions.com>
2025-07-24 08:48:18 -0400

    attempt at converting the csv elmhurst had into a user_lists.json we could use in importAspenData

---
+++ code/web/services/ETL/Home.php
@@ -0,0 +1,47 @@
+<?php
+require_once ROOT_DIR . '/Action.php';
+
+class ETL_Home extends Action {
+	function launch() {
+		global $serverName;
+		$importPath = '/data/aspen-discovery/' . $serverName . '/import/';
+		$importFile = $importPath . "list.csv";
+		$fileHandler = fopen($importFile, 'r');
+		$line = fgets($fileHandler);
+		$createdLists = array();
+		while ($line) {
+			$split = explode(",",$line);
+			$barcode = trim($split[0]);
+			$patronID = trim($split[1]);
+			$ListName = trim($split[2]);
+			$BibID = trim($split[3]);
+			if(!array_key_exists($ListName, $createdLists))
+			{
+				$temp = array("user"=>$barcode, "user_id"=>$patronID, "title" => $ListName,
+				"links" => array("userListEntries" => []));
+				$createdLists[$ListName] = $temp;
+			}
+			$createdLists[$ListName]["links"]["userListEntries"][] = array("source"=> "GroupedWork", "sourceId" => $BibID);
+			$line = fgets($fileHandler);
+		}
+		fclose($fileHandler);
+		$exportFile = $importPath . "user_lists.json";
+		$exportFileHandler = fopen($exportFile, 'w');
+
+		foreach($createdLists as $name => $list)
+		{
+			fwrite($exportFileHandler, json_encode($list) . "\n");
+			echo json_encode($list) . "<br>";
+		}
+		fclose($exportFileHandler);
+		//global $interface;
+		//$this->display('default.tpl', 'Hello World!', false);
+	}
+
+	function getBreadcrumbs(): array
+	{
+		$breadcrumbs = [];
+		return $breadcrumbs;
+	}
+}
+?>
