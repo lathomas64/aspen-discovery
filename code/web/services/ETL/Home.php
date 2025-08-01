96934cbd5bdc3b7be4dad5fd85b4d5fada308721
Imani <imani.thomas@bywatersolutions.com>
2025-08-01 13:54:16 -0400

    Restructure json result putting user inside of links

--- code/web/services/ETL/Home.php
+++ code/web/services/ETL/Home.php
@@ -1,24 +1,31 @@
 <?php
 require_once ROOT_DIR . '/Action.php';
+require_once ROOT_DIR . '/RecordDrivers/RecordDriverFactory.php';
 
 class ETL_Home extends Action {
 	function launch() {
 		global $serverName;
 		$importPath = '/data/aspen-discovery/' . $serverName . '/import/';
 		$importFile = $importPath . "list.csv";
+		if(!file_exists($importFile))
+		{
+			echo "could not find $importFile";
+			return;
+		}
 		$fileHandler = fopen($importFile, 'r');
 		$line = fgets($fileHandler);
 		$createdLists = array();
 		while ($line) {
 			$split = explode(",",$line);
 			$barcode = trim($split[0]);
-			$patronID = trim($split[1]);
+			//$patronID = trim($split[1]);
 			$ListName = trim($split[2]);
 			$BibID = trim($split[3]);
 			if(!array_key_exists($ListName, $createdLists))
 			{
-				$temp = array("user"=>$barcode, "user_id"=>$patronID, "title" => $ListName,
-				"links" => array("userListEntries" => []));
+				$temp = array("title" => $ListName,
+					"importedFrom" => "Sierra",
+					"links" => array("user" =>$barcode, "userListEntries" => []));
 				$createdLists[$ListName] = $temp;
 			}
 			$createdLists[$ListName]["links"]["userListEntries"][] = array("source"=> "GroupedWork", "sourceId" => $BibID);
