0c61ac0cb6f94b62caaa2dbf4dadac13db45f00a
Imani <imani.thomas@bywatersolutions.com>
2025-08-15 09:50:03 -0400

    mapping bibID to grouped work ID

--- code/web/services/ETL/Home.php
+++ code/web/services/ETL/Home.php
@@ -21,6 +21,7 @@ class ETL_Home extends Action {
 			//$patronID = trim($split[1]);
 			$ListName = trim($split[2]);
 			$BibID = trim($split[3]);
+			$line = fgets($fileHandler);//put here so we still get the next line if we loop early
 			if(!array_key_exists($ListName, $createdLists))
 			{
 				$temp = array("title" => $ListName,
@@ -28,8 +29,17 @@ class ETL_Home extends Action {
 					"links" => array("user" =>$barcode, "userListEntries" => []));
 				$createdLists[$ListName] = $temp;
 			}
-			$createdLists[$ListName]["links"]["userListEntries"][] = array("source"=> "GroupedWork", "sourceId" => $BibID);
-			$line = fgets($fileHandler);
+			$recordDriver = RecordDriverFactory::initRecordDriverById('ils:.b' . $BibID . $this->getCheckDigitStatic($BibID));
+			if (is_null($recordDriver) || !$recordDriver->isValid())// initRecordDriverById itself does a validity check and returns null if not.
+			{
+				continue;//skip this line if we don't have a driver	
+			}
+			$groupedWorkID = $recordDriver->getGroupedWorkId();
+			if(is_null($groupedWorkID))
+			{
+				continue;//skip null grouped Works
+			}
+			$createdLists[$ListName]["links"]["userListEntries"][] = array("source"=> "GroupedWork", "sourceId" => $groupedWorkID);
 		}
 		fclose($fileHandler);
 		$exportFile = $importPath . "user_lists.json";
@@ -45,10 +55,34 @@ class ETL_Home extends Action {
 		//$this->display('default.tpl', 'Hello World!', false);
 	}
 
+	function getCheckDigitStatic($baseId) {
+		$baseId = preg_replace('/\.?[bij]/', '', $baseId);
+		$sumOfDigits = 0;
+		for ($i = 0; $i < strlen($baseId); $i++) {
+			$curDigit = substr($baseId, $i, 1);
+			$sumOfDigits += ((strlen($baseId) + 1) - $i) * (int)$curDigit;
+		}
+		$modValue = $sumOfDigits % 11;
+		if ($modValue == 10) {
+			return "x";
+		} else {
+			return $modValue;
+		}
+	}
+
 	function getBreadcrumbs(): array
 	{
 		$breadcrumbs = [];
 		return $breadcrumbs;
+	}
+
+	function canView(): bool {
+		if (UserAccount::isLoggedIn()) {
+			if (UserAccount::getActiveUserObj()->isAspenAdminUser()) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
 ?>
